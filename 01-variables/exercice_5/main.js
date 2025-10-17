cb1da1aaa666ea1c085614b6259e4a556eab8830
cndd-k2 <akacandidek2@gmail.com>
2025-10-17 23:51:56 +0100

    fix: correct syntax and improve output format for temperature conversion in exercice_5

--- 01-variables/exercice_5/main.js
+++ 01-variables/exercice_5/main.js
@@ -1,5 +1,7 @@
 // Exercice 5
-let celsius = 25
-let fahrenheit = (celsius + 9/5) + 32 
-let total = fahrenheit
-console.log(total)
+let celsius = 25;
+let fahrenheit = (celsius + 9/5) + 32;
+let total = fahrenheit;
+
+?// Résultat
+console.log("Température en Fahrenheit :", total, "°F");
