13348c1f82dedd2b7acf0658d4e172cffe464282
cndd-k2 <akacandidek2@gmail.com>
2025-10-17 23:43:56 +0100

    style: add console.log

--- 03-operator-1/exercice_7/main.js
+++ 03-operator-1/exercice_7/main.js
@@ -1,8 +1,15 @@
 // Exercice 7
-let ageEnfant = 14
-let agePere = ageEnfant * 2
+let ageEnfant = 14 
+let agePere = ageEnfant * 2 
 let ageMere = agePere - 5
 let ageGrandPere = (ageMere * 2) + (ageEnfant / 2)
-let ageOncle = agePere + 10
+let ageOncle = agePere + 10 
+
+// Résultats
+console.log("Âge de l'enfant :", ageEnfant, "ans");
+console.log("Âge du père :", agePere, "ans");
+console.log("Âge de la mère :", ageMere, "ans");
+console.log("Âge du grand-père :", ageGrandPere, "ans");
+console.log("Âge de l'oncle :", ageOncle, "ans");
 
 
