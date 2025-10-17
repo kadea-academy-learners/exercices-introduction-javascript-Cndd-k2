9bbd63aeca2c7552fe9a48c118e89007d9d6ebe0
cndd-k2 <akacandidek2@gmail.com>
2025-10-17 18:29:20 +0100

    calculation of the percentage of each value on 03-operator-1

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -7,11 +7,30 @@ const transport = salaireMensuel * 0.1;
 let autresDepenses = 50;
     autresDepenses += 25;
     loisirs = salaireMensuel * 0.15;
+
+// calcul des depenses et reste
 let totalDepenses = loyer + nourriture + transport + autresDepenses + loisirs;
 let reste = salaireMensuel - totalDepenses;
 
+// affichage des resultats
 console.log(totalDepenses)
 console.log(reste)
+
+// Calcul des pourcentages
+let loyerPourcentage = (loyer / salaireMensuel) * 100;
+let nourriturePourcentage = (nourriture /salaireMensuel) * 100;
+let transportPourcentage = (transport / salaireMensuel) * 100;
+let autresDepensesPourcentage = (autresDepenses / salaireMensuel) * 100;
+let loisirsPourcentage = (loisirs / salaireMensuel) * 100;
+
+// Affichage des résultats pourcentage
+console.log("Loyer : " + loyer + " (" + loyerPourcentage + "% du salaire)");
+console.log("Nourriture : " + nourriture + " (" + nourriturePourcentage + "% du salaire)");
+console.log("Transport : " + transport + " (" + transportPourcentage + "% du salaire)");
+console.log("Autres depenses : " + autresDepenses + " (" + autresDepensesPourcentage + "% du salaire)");
+console.log("Loisirs : " + loisirs + " (" + loisirsPourcentage + "% du salaire)");
+
+
 
 function verifierReste(reste){
     if(reste>=100){
