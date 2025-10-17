133ddd70fd586757eb73fae4f55e6a5d75ccd724
cndd-k2 <akacandidek2@gmail.com>
2025-10-17 23:50:03 +0100

    refactor: display variable types in consol

--- 02-type-donnes/exercice_6/main.js
+++ 02-type-donnes/exercice_6/main.js
@@ -1,6 +1,13 @@
 // Exercice 6
-let age = 10
-let nom = "lolita"
-let estConnecte = false
-let valeurNulle = null
-let unknow 
+let age = 24;
+let nom = "lolita";
+let estConnecte = false;
+let valeurNulle = null;
+let unknow;
+
+// Affichage des types
+console.log("Type de la variable age :", typeof age);                   
+console.log("Type de la variable nom :", typeof nom);
+console.log("Type de la variable estConnecte :", typeof estConnecte);
+console.log("Type de la variable valeurNulle :", typeof valeurNulle);     
+console.log("Type de la variable unknow :", typeof unknow);
