0b7bd8376e252f0f4d4ccb00b19625ef0d6b57d6
cndd-k2 <akacandidek2@gmail.com>
2025-10-17 18:09:43 +0100

    completed exercise 8 on budget management on 03-operator-1

--- 03-operator-1/exercice_8/main.js
+++ 03-operator-1/exercice_8/main.js
@@ -1,2 +1,33 @@
 // Exercice 8
-// Écrivez votre code ici
+const salaireMensuel = 500;
+const loyer = salaireMensuel * 0.3;
+const nourriture = salaireMensuel * 0.2;
+const transport = salaireMensuel * 0.1;
+
+let autresDepenses = 50;
+    autresDepenses += 25;
+let totalDepenses = loyer + nourriture + transport + autresDepenses;
+let reste = salaireMensuel - totalDepenses;
+
+console.log(totalDepenses)
+console.log(reste)
+
+function verifierReste(reste){
+    if(reste>=100){
+        console.log("budget bien gere");
+    }else{
+        console.log("attention serre")
+    }
+}
+verifierReste(100);
+
+function calcultotalDepenses(salaire, depenses){
+    let reste = salaire - depenses;
+    for(let i=1; i<=3; i++){
+        console.log("votre reste est : " + reste)
+    }
+}
+
+
+
+
